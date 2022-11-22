c030e2edc25431cecd04db15297f1f7b1254fe34
Adam <adamlederer@live.ca>
2022-11-21 22:58:35 -0700

    struggled with deleting of cookie. Need more knowlege on it. Hoping to learn how to setup array of objects when user clicks.

--- appjs/app.js
+++ appjs/app.js
@@ -16,19 +16,19 @@ let game1 = {
     title: `Pokemon Scarlet`,
     price: 80,
     image: `https://assets-prd.ignimgs.com/2022/08/03/pokemon-scarlet-1659542292648.jpg`,
-    descript: ``
+    descript: `Pokemon game that has is the other half of Pokemon Violet`
 }
 let game2 = {
     title: `Minecraft`,
     price: 40,
     image: `https://compass-ssl.xbox.com/assets/13/07/1307d85d-e58d-44b6-8734-22577a44941e.jpg?n=Minecraft-2017_sharing_200x200.jpg`,
-    descript: ``
+    descript: `A building game that is made for all ages`
 }
 let game3 = {
     title: `Modern Warfare 2 Vault Edition`,
     price: 130,
     image: `https://image.api.playstation.com/vulcan/ap/rnd/202205/2800/1hUaZ2xTIXDRch0KRb3Bzsgf.png`,
-    descript: ``
+    descript: `A Mature rated game for those who like combat and shooting.`
 }
 
 
@@ -36,22 +36,25 @@ let game3 = {
 
 
 let selection1 = document.getElementById(`option1`);
-selection1.insertAdjacentHTML(`afterbegin`, `<h2>${game1.title}</h2>`);
+selection1.insertAdjacentHTML(`afterbegin`, `<p>${game1.descript}</p>`);
 selection1.insertAdjacentHTML(`afterbegin`, `<img src="${game1.image}" alt=""</>`);
 selection1.insertAdjacentHTML(`afterbegin`, `<p>Price: ${game1.price}</p>`);
-selection1.insertAdjacentHTML(`afterbegin`, `<p>${game1.descript}</p>`);
+selection1.insertAdjacentHTML(`afterbegin`, `<h2>${game1.title}</h2>`);
+
 
 selection2 = document.getElementById(`option2`);
-selection2.insertAdjacentHTML(`afterbegin`, `<h2>${game2.title}</h2>`);
+selection2.insertAdjacentHTML(`afterbegin`, `<p>${game2.descript}</p>`);
 selection2.insertAdjacentHTML(`afterbegin`, `<img src="${game2.image}" alt=""</>`);
 selection2.insertAdjacentHTML(`afterbegin`, `<p>Price: ${game2.price}</p>`);
-selection2.insertAdjacentHTML(`afterbegin`, `<p>${game2.descript}</p>`);
+selection2.insertAdjacentHTML(`afterbegin`, `<h2>${game2.title}</h2>`);
+
 
 selection3 = document.getElementById(`option3`);
-selection3.insertAdjacentHTML(`afterbegin`, `<h2>${game3.title}</h2>`);
+selection3.insertAdjacentHTML(`afterbegin`, `<p>${game3.descript}</p>`);
 selection3.insertAdjacentHTML(`afterbegin`, `<img src="${game3.image}" alt=""</>`);
 selection3.insertAdjacentHTML(`afterbegin`, `<p>Price: ${game3.price}</p>`);
-selection3.insertAdjacentHTML(`afterbegin`, `<p>${game3.descript}</p>`);
+selection3.insertAdjacentHTML(`afterbegin`, `<h2>${game3.title}</h2>`);
+
 
 let choices = []
 selection1.querySelector(`button`).addEventListener(`click`, choiceHandler);
--- appjs/checkout.js
+++ appjs/checkout.js
@@ -1,8 +1,9 @@
 let selectionJson = Cookies.get(`gameSelect`);
 let select = JSON.parse(selectionJson);
-select.price *= 0.2;
 
-document.body.insertAdjacentHTML(`afterbegin`, `<h2>${select.title}</h2>`);
+
+document.body.insertAdjacentHTML(`afterbegin`, `<p>${select.descript}</p>`);
 document.body.insertAdjacentHTML(`afterbegin`, `<img src="${select.image}" alt="Game Image"</>`);
 document.body.insertAdjacentHTML(`afterbegin`, `<p>Price: ${select.price}</p>`);
-document.body.insertAdjacentHTML(`afterbegin`, `<p>${select.description}</p>`);
+document.body.insertAdjacentHTML(`afterbegin`, `<h2>${select.title}</h2>`);
+
